0225d9d2cf92f59561cf087199619819f49c0bd0
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2023-04-05 09:30:20 +0200

    Update src/Controller/AdminOrderController.php
    Co-authored-by: Martin Eiber <martin.eiber@pimcore.com>

--- src/Controller/AdminOrderController.php
+++ src/Controller/AdminOrderController.php
@@ -175,7 +175,7 @@ class AdminOrderController extends AdminController implements KernelControllerEv
         // Paginate the results of the query
         $paginator = $paginator->paginate(
             $list,
-            (int) $request->get('page', 1),
+            $request->query->getInt('page', 1),
             10
         );
 
