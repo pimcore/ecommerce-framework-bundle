da465d2d6a4ab3fe9c469a9fd85ae725b032f1cb
Martin Eiber <martin.eiber@pimcore.com>
2023-04-11 15:42:34 +0200

    fix type error. (#24)

--- src/Controller/AdminOrderController.php
+++ src/Controller/AdminOrderController.php
@@ -467,7 +467,7 @@ class AdminOrderController extends AdminController implements KernelControllerEv
 
             // change item
             $agent = $this->orderManager->createOrderAgent($order);
-            $note = $agent->itemComplaint($orderItem, $request->get('quantity'));
+            $note = $agent->itemComplaint($orderItem, (float) $request->get('quantity'));
 
             // extend log
             $note->addData('message', 'text', $request->get('message'));
