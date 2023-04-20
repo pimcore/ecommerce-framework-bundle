d831e6e1b2f2c79ad4da8e03657c78cf6071f4ec
Matthias Schuhmayer <38959016+mattamon@users.noreply.github.com>
2023-04-20 14:44:00 +0200

    [Task] Setup codeception (#39)
    * Add codeception
    
    * Change file permissions
    
    * Add admin bundle
    
    * Update system.yaml
    
    * Update security.yaml
    
    * Updating codeception ruleset
    
    Co-authored-by: JiaJia Ji <kingjia90@gmail.com>
    
    * Update system.yaml remove asset configs
    
    ---------
    
    Co-authored-by: JiaJia Ji <kingjia90@gmail.com>

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -34,22 +34,17 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 
 $pimcoreTestsSupportDir = $pimcoreTestDir . '/Support';
 
-//Pimcore 10 BC layer
-if (!is_dir($pimcoreTestsSupportDir)) {
-    $pimcoreTestsSupportDir = $pimcoreTestDir . '/_support';
-}
-
 include $pimcoreTestsSupportDir . '/Util/Autoloader.php';
 
 \Pimcore\Bootstrap::setProjectRoot();
 \Pimcore\Bootstrap::bootstrap();
 
 //error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
-Autoloader::addNamespace('Pimcore\Tests', $pimcoreTestsSupportDir);
 Autoloader::addNamespace('Pimcore\Tests\Support', $pimcoreTestsSupportDir);
 
 Autoloader::addNamespace('Pimcore\Model\DataObject', PIMCORE_CLASS_DIRECTORY . '/DataObject');
 Autoloader::addNamespace('Pimcore\Bundle\EcommerceFrameworkBundle\Tests', __DIR__);
+Autoloader::addNamespace('Pimcore\Bundle\EcommerceFrameworkBundle\Tests', __DIR__ . '/_support');
 
 if (!defined('TESTS_PATH')) {
     define('TESTS_PATH', __DIR__);
