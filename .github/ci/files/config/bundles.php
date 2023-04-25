467dd1848a38448f5798cf62b1a7838d962cdb11
mattamon <matthias.schuhmayer@pimcore.com>
2023-04-25 13:17:11 +0200

    Remove Admin from bundles

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -1,6 +1,5 @@
 <?php
 
 return [
-    "Pimcore\\Bundle\\AdminBundle\\PimcoreAdminBundle" => ['all' => true],
     "Pimcore\\Bundle\\EcommerceFrameworkBundle\\PimcoreEcommerceFrameworkBundle" =>  ['all' => true],
 ];
