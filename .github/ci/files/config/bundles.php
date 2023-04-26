a9f91538ab84c1be2634698164a3236f71c5d6b1
Divesh Pahuja <divesh.pahuja@pimcore.com>
2023-04-26 10:30:59 +0200

    re-enable admin bundle for tests

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
+    "Pimcore\\Bundle\\AdminBundle\\PimcoreAdminBundle" => ['all' => true],
     "Pimcore\\Bundle\\EcommerceFrameworkBundle\\PimcoreEcommerceFrameworkBundle" =>  ['all' => true],
 ];
