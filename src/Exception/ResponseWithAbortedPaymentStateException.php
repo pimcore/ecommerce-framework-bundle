cd27e4fee8cf7d1580f864ea7f0d706750278d26
Florian Mysliwietz <florian.mysliwietz@elements.at>
2023-05-16 10:55:35 +0200

    Improve exception for orders with aborted payment state (#64)
    * Improve exception for orders with aborted payment state
    
    * retrigger checks
    
    * Improve exception for orders with aborted payment state

---
+++ src/Exception/ResponseWithAbortedPaymentStateException.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\EcommerceFrameworkBundle\Exception;
+
+class ResponseWithAbortedPaymentStateException extends UnsupportedException
+{
+    protected ?string $paymentState;
+
+    public function __construct(?string $newPaymentState)
+    {
+        $message = 'Got response although payment state was already aborted, new payment state was ' . $newPaymentState;
+        parent::__construct($message);
+        $this->paymentState = $newPaymentState;
+    }
+
+    public function getPaymentState(): ?string
+    {
+        return $this->paymentState;
+    }
+}
