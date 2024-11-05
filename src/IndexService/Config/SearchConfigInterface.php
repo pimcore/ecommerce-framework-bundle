64c70b372f48aeff4b24e3c7a93e89dffe050243
mattamon <matthias.schuhmayer@pimcore.com>
2024-11-05 16:25:33 +0100

    Remove deprication message and add method to interface

--- src/IndexService/Config/SearchConfigInterface.php
+++ src/IndexService/Config/SearchConfigInterface.php
@@ -18,4 +18,5 @@ namespace Pimcore\Bundle\EcommerceFrameworkBundle\IndexService\Config;
 
 interface SearchConfigInterface extends ConfigInterface
 {
+    public function getClientConfig(string $setting = null): array|string|null;
 }
