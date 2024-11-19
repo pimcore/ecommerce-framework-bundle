7bda5e2dffe1810bf464b15d69344b490866de2f
ctippler <ctippler@users.noreply.github.com>
2024-11-19 13:59:40 +0000

    Apply php-cs-fixer changes

--- src/PricingManager/Rule/Dao.php
+++ src/PricingManager/Rule/Dao.php
@@ -9,8 +9,8 @@
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     GPLv3 and PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
 namespace Pimcore\Bundle\EcommerceFrameworkBundle\PricingManager\Rule;
@@ -86,6 +86,7 @@ class Dao extends AbstractDao
             $this->db->commit();
         } catch (\Exception $e) {
             $this->db->rollBack();
+
             throw $e;
         }
     }
