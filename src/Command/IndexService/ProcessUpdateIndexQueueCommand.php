93cea832717c0303f9926b26098adc0e9c5fa76f
Julian <julian.nuernberger@valantic.at>
2025-05-26 09:20:36 +0200

    Support for adjusting batch-size in ProcessUpdateIndexQueueCommand (#229)
    * Update ProcessUpdateIndexQueueCommand.php
    
    * Apply php-cs-fixer changes
    
    * Update ProcessUpdateIndexQueueCommand.php
    
    * Apply php-cs-fixer changes
    
    * Update ProcessUpdateIndexQueueCommand.php
    
    ---------
    
    Co-authored-by: jremmurd <9052094+jremmurd@users.noreply.github.com>

--- src/Command/IndexService/ProcessUpdateIndexQueueCommand.php
+++ src/Command/IndexService/ProcessUpdateIndexQueueCommand.php
@@ -152,8 +152,28 @@ class ProcessUpdateIndexQueueCommand extends AbstractIndexServiceCommand
         return 'combined product ID rows in store table index';
     }
 
+    /**
+     * index updates per child process
+     */
     protected function getSegmentSize(): int
     {
-        return 500; // index updates per child process
+        $segmentSize = 500;
+
+        if ($this->input->hasOption('segment-size') && $this->input->getOption('segment-size')) {
+            $segmentSize = (int)$this->input->getOption('segment-size');
+        }
+
+        return $segmentSize;
+    }
+
+    protected function getBatchSize(): int
+    {
+        $batchSize = $this->getSegmentSize();
+
+        if ($this->input->hasOption('batch-size') && $this->input->getOption('batch-size')) {
+            $batchSize = (int)$this->input->getOption('batch-size');
+        }
+
+        return $batchSize;
     }
 }
