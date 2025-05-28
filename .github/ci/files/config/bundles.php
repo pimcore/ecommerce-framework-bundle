1b0d310b479b35751cbc5d45abc415a28d2461a0
Bernhard Rusch <brusch@users.noreply.github.com>
2025-05-28 09:38:01 +0200

    🔄 synced file(s) with pimcore/sync-common-files (#259)
    * 🔄 synced file(s) with pimcore/sync-common-files
    
    * Update bundles.php
    
    ---------
    
    Co-authored-by: mcop1 <89011527+mcop1@users.noreply.github.com>

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     "Pimcore\\Bundle\\AdminBundle\\PimcoreAdminBundle" => ['all' => true],
     "Pimcore\\Bundle\\EcommerceFrameworkBundle\\PimcoreEcommerceFrameworkBundle" =>  ['all' => true],
+    'Pimcore\\Bundle\\SeoBundle\\PimcoreSeoBundle' => ['all' => true],
 ];
