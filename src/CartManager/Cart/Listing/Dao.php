748ede1d59c20cb8464d8061765d2dbbeabfd5d2
Jacob Dreesen <j.dreesen@neusta.de>
2025-06-04 14:02:09 +0200

    Update Dao.php

--- src/CartManager/Cart/Listing/Dao.php
+++ src/CartManager/Cart/Listing/Dao.php
@@ -27,8 +27,10 @@ class Dao extends \Pimcore\Model\Listing\Dao\AbstractDao
     public function load(): array
     {
         $carts = [];
-        $cartIds = $this->db->fetchFirstColumn('SELECT id FROM ' . \Pimcore\Bundle\EcommerceFrameworkBundle\CartManager\Cart\Dao::TABLE_NAME .
-                                                 $this->getCondition() . $this->getOrder() . $this->getOffsetLimit(), $this->model->getConditionVariables());
+        $cartIds = $this->db->fetchFirstColumn(
+            'SELECT id FROM ' . \Pimcore\Bundle\EcommerceFrameworkBundle\CartManager\Cart\Dao::TABLE_NAME . $this->getCondition() . $this->getOrder() . $this->getOffsetLimit(), 
+            $this->model->getConditionVariables(),
+        );
 
         foreach ($cartIds as $id) {
             $carts[] = call_user_func([$this->getCartClass(), 'getById'], $id);
@@ -42,7 +44,10 @@ class Dao extends \Pimcore\Model\Listing\Dao\AbstractDao
     public function getTotalCount(): int
     {
         try {
-            return (int) $this->db->fetchOne('SELECT COUNT(*) FROM `' . \Pimcore\Bundle\EcommerceFrameworkBundle\CartManager\Cart\Dao::TABLE_NAME . '`' . $this->getCondition(), $this->model->getConditionVariables());
+            return (int) $this->db->fetchOne(
+                'SELECT COUNT(*) FROM `' . \Pimcore\Bundle\EcommerceFrameworkBundle\CartManager\Cart\Dao::TABLE_NAME . '`' . $this->getCondition(), 
+                $this->model->getConditionVariables(),
+            );
         } catch (\Exception $e) {
             return 0;
         }
