4173c14fc972b16fa1adb3bbdeacbdbaf39a08f0
m.binerbay <murat.binerbay@alfatraining.de>
2025-08-06 20:21:02 +0200

    Add getRealFieldname to AbstractElasticsearch

--- src/IndexService/ProductList/ElasticSearch/AbstractElasticSearch.php
+++ src/IndexService/ProductList/ElasticSearch/AbstractElasticSearch.php
@@ -16,6 +16,7 @@ namespace Pimcore\Bundle\EcommerceFrameworkBundle\IndexService\ProductList\Elast
 use Elastic\Elasticsearch\Client;
 use Pimcore\Bundle\EcommerceFrameworkBundle\Exception\InvalidConfigException;
 use Pimcore\Bundle\EcommerceFrameworkBundle\Factory;
+use Pimcore\Bundle\EcommerceFrameworkBundle\FilterService\FilterType\AbstractFilterType;
 use Pimcore\Bundle\EcommerceFrameworkBundle\IndexService\Config\ElasticSearch;
 use Pimcore\Bundle\EcommerceFrameworkBundle\IndexService\Config\SearchConfigInterface;
 use Pimcore\Bundle\EcommerceFrameworkBundle\IndexService\ProductList\ProductListInterface;
@@ -165,6 +166,7 @@ abstract class AbstractElasticSearch implements ProductListInterface, TenantConf
      */
     public function addCondition(array|string|bool $condition, string $fieldname = ''): void
     {
+        $fieldname = $this->getRealFieldname($fieldname);
         $this->filterConditions[$fieldname][] = $condition;
         $this->preparedGroupByValuesLoaded = false;
         $this->products = null;
@@ -187,6 +189,7 @@ abstract class AbstractElasticSearch implements ProductListInterface, TenantConf
      */
     public function addRelationCondition(string $fieldname, string|array $condition): void
     {
+        $fieldname = $this->getRealFieldname($fieldname);
         $this->relationConditions[$fieldname][] = $condition;
         $this->preparedGroupByValuesLoaded = false;
         $this->products = null;
@@ -1236,5 +1239,18 @@ abstract class AbstractElasticSearch implements ProductListInterface, TenantConf
         }
 
         return 0.0;
+    }
+
+    /**
+     * Returns fieldname without precondition prefix.
+     */
+    protected function getRealFieldname(string $fieldname): string
+    {
+        $isPrecondition = str_starts_with($fieldname, AbstractFilterType::PREFIX_PRECONDITION);
+        if ($isPrecondition) {
+            return substr($fieldname, strlen(AbstractFilterType::PREFIX_PRECONDITION));
+        }
+
+        return $fieldname;
     }
 }
