e76bb4ff9bbd15c7ef33ec7cbbc1dcbaa143a607
Andres Sanz <andres.sanz@arrabiata.de>
2025-09-17 10:45:37 +0200

    issue 264 fix Bracket function getConditionsByType

--- src/PricingManager/Condition/Bracket.php
+++ src/PricingManager/Condition/Bracket.php
@@ -149,7 +149,7 @@ class Bracket implements BracketInterface
 
         foreach ($this->conditions as $condition) {
             if ($condition instanceof BracketInterface) {
-                $conditions = array_merge($condition->getConditionsByType($typeClass));
+                $conditions = array_merge($conditions, $condition->getConditionsByType($typeClass));
             } elseif ($condition instanceof $typeClass) {
                 $conditions[] = $condition;
             }
