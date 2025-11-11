433521e373b4a99ba40bea22cce8da0fd89aa4b0
JiaJia Ji <kingjia90@gmail.com>
2025-11-11 12:58:42 +0100

    Cast cart ID to string in condition

--- src/CartManager/CartItem.php
+++ src/CartManager/CartItem.php
@@ -107,7 +107,7 @@ class CartItem extends AbstractCartItem implements CartItemInterface
             $itemList->setCartItemClassName(get_class($this));
 
             $db = \Pimcore\Db::get();
-            $itemList->setCondition('cartId = ' . $db->quote($this->getCartId()) . ' AND parentItemKey = ' . $db->quote($this->getItemKey()));
+            $itemList->setCondition('cartId = ' . $db->quote((string)$this->getCartId()) . ' AND parentItemKey = ' . $db->quote($this->getItemKey()));
 
             foreach ($itemList->getCartItems() as $item) {
                 if ($item->getProduct() != null) {
